bc81314d851d77a2367f6e6677b01bd26121a29a
rdunk <rupert@zakgroup.co.uk>
2018-08-16 14:49:29 +0100

    switch to lib, dont uglify, remove dist

--- build.js
+++ build.js
@@ -1,25 +1,21 @@
+const pkg = require('./package.json');
 const rollup = require('rollup');
 const babel = require('rollup-plugin-babel');
-const uglify = require('rollup-plugin-uglify');
 
 const inputOptions = {
   input: 'src/index.js',
-  plugins: [
-    babel(),
-    uglify(),
-  ],
+  plugins: [babel()],
 };
 
 const outputOptions = {
   name: 'VuePacker',
-  file: 'dist/vue-packer.js',
+  file: pkg.main,
   format: 'cjs',
 };
 
 async function build() {
   const bundle = await rollup.rollup(inputOptions);
   // generate code and sourcemap
-  // const { code, map } = await bundle.generate(outputOptions);
   await bundle.write(outputOptions);
 }
 
